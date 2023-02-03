985c63fafca75b118e9fa234b1beccd0754e85c5
Andrew Nourry <andrewnourry@gmail.com>
2023-02-03 15:05:54 +0100

    test added for Lettings view

--- lettings/tests.py
+++ lettings/tests.py
@@ -1,6 +1,8 @@
 import pytest
 from django.urls import reverse
 from django.test import Client
+from pytest_django.asserts import assertTemplateUsed
+from .models import Address, Letting
 
 
 @pytest.fixture
@@ -12,4 +14,32 @@ def client():
 def test_index(client):
     url = reverse("lettings:index")
     response = client.get(url)
+
+    content = response.content.decode()
+    expected_content = "<title>Lettings</title>"
+
     assert response.status_code == 200
+    assert expected_content in content
+
+
+@pytest.mark.django_db
+def test_lettings():
+    client = Client()
+
+    adress = Address.objects.create(
+        number=1,
+        street="sesame street",
+        city="Los Angeles",
+        state="LA",
+        zip_code=75012,
+        country_iso_code="USA",
+    )
+    Letting.objects.create(title="Beautifull place", address=adress)
+    path = reverse("lettings:lettings", kwargs={"letting_id": 1})
+    response = client.get(path)
+    content = response.content.decode()
+    expected_content = "<title>Beautifull place</title>"
+
+    assert expected_content in content
+    assert response.status_code == 200
+    assertTemplateUsed(response, "lettings/letting.html")
