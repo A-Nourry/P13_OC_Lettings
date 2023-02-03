f1a25c8b5a24bc0fe445dfa6f6afea96adfe61da
Andrew Nourry <andrewnourry@gmail.com>
2023-02-03 15:06:16 +0100

    test added for profile view

--- profiles/tests.py
+++ profiles/tests.py
@@ -1,6 +1,9 @@
 import pytest
 from django.urls import reverse
 from django.test import Client
+from django.contrib.auth.models import User
+from .models import Profile
+from pytest_django.asserts import assertTemplateUsed
 
 
 @pytest.fixture
@@ -12,4 +15,28 @@ def client():
 def test_index(client):
     url = reverse("profiles:index")
     response = client.get(url)
+    content = response.content.decode()
+    expected_content = "<title>Profiles</title>"
+
     assert response.status_code == 200
+    assert expected_content in content
+
+
+@pytest.mark.django_db
+def test_profiles():
+    client = Client()
+
+    user = User.objects.create_user(
+        username="test_username",
+        email="test@gmail.com",
+        password="password",
+    )
+    Profile.objects.create(user=user, favorite_city="Paris")
+    path = reverse("profiles:profile", kwargs={"username": user.username})
+    response = client.get(path)
+    content = response.content.decode()
+    expected_content = "<title>test_username</title>"
+
+    assert expected_content in content
+    assert response.status_code == 200
+    assertTemplateUsed(response, "profiles/profile.html")
