534debd89f28c1c41c358f61b0a1e59f9036ff8d
Andrew Nourry <andrewnourry@gmail.com>
2023-02-03 15:06:02 +0100

    test updated

--- oc_lettings_site/tests.py
+++ oc_lettings_site/tests.py
@@ -12,4 +12,8 @@ def client():
 def test_index(client):
     url = reverse("index")
     response = client.get(url)
+    content = response.content.decode()
+    expected_content = "<title>Holiday Homes</title>"
+
     assert response.status_code == 200
+    assert expected_content in content
